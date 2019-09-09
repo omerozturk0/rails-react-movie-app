903d3328e7358f45f05e20b25baaafd2c6c7c27e
Ömer Öztürk <omer.oztrk0@gmail.com>
2019-09-09 12:29:44 +0300

    heroku issue

--- app/javascript/packs/server_rendering.js
+++ app/javascript/packs/server_rendering.js
@@ -1,7 +1,3 @@
-// By default, this pack is loaded for server-side rendering.
-// It must expose react_ujs as `ReactRailsUJS` and prepare a require context.
 //= require react-server
 //= require components
-var componentRequireContext = require.context("components", true);
-var ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
+// Require everything needed for server rendering
